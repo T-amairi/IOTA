ee3c446f0a71ea3e4a83c4d3883b110d9a6fe9e9
T-amairi <amairitar@gmail.com>
2021-06-28 00:06:57 +0200

    added image & log subdirectories

--- data/Stats.py
+++ data/Stats.py
@@ -16,19 +16,24 @@ for log in logs:
         t.append(float(m[-1]))
 
     l.close()
-    #os.remove(log)
     
 t = float(sum(t)/len(t))
 print("Average time of execution:",t,"seconds")
 
+path = r".\data\tracking"
+os.chdir(path)
+
 NbTips = 0
-TipsFile = open(glob.glob("Number*.txt")[0],'r')
+TipsFile = glob.glob("Number*.txt")
+NbModule = 0
 
-for line in TipsFile.readlines():
-    NbTips += int(line)
+for file in TipsFile:
+    NbTips = 0
+    f = open(file,'r')
+    for line in f.readlines():
+        NbTips += int(line)
 
-NbTips = NbTips/Nbrun
-print("Average number of tips:",NbTips)
-
-TipsFile.close()
-#os.remove(glob.glob("Number*.txt")[0])
+    NbTips = NbTips/Nbrun
+    print("Average number of tips for NodeModule[" + str(NbModule) "]:",NbTips)
+    NbModule += 1
+    f.close()
